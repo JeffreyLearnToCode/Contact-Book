0ab02c2d4c5011006be9fdb29f3a2fb80f16932a
Jeffrey Abraham <jeffrey@abrahamjeffrey.com>
2025-11-03 21:48:12 -0500

    Implement getLine this will help when getting information from the user. Need to ticker a bit more with string_view and string. Build a better understanding of [[nodiscard]]

--- Menu.cpp
+++ Menu.cpp
@@ -7,6 +7,16 @@
 Menu::Menu(const std::pmr::vector<std::string_view> &option, int offset)
     : option_(option), offset_(offset) {}
 
+Menu::Menu() {}
+
+void Menu::setOption(const std::pmr::vector<std::string_view> &option) {
+    this->option_ = option;
+}
+
+void Menu::setOffset(const int &offset) {
+    this->offset_ = offset;
+}
+
 void Menu::printOption() const {
     for (int i = 0; i < option_.size(); i++) {
         std::cout << (i+this->offset_) << ". " << option_[i] << std::endl;
@@ -20,6 +30,21 @@ int Menu::choiceOption() const {
     for (;;) {
         this->printOption();
         if (int d; std::cin >> d && low <= d && d <= high) {
+            return d;
+        }
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Please pick the correct options" << std::endl;
+    }
+}
+
+std::string Menu::getLine(const std::string_view question) {
+    for (;;) {
+        std::cout << question << ": ";
+
+        if (std::string d; std::getline(std::cin, d) &&
+            !d.empty()) {
             return d;
         }
 
--- Menu.h
+++ Menu.h
@@ -16,10 +16,15 @@ class Menu {
 
 public:
     Menu(const std::pmr::vector<std::string_view> &option, int offset);
+    Menu();
 
     void printOption() const;
+    void setOption(const std::pmr::vector<std::string_view> &option);
+    void setOffset(const int &offset);
 
     [[nodiscard]] int choiceOption() const;
+
+    [[nodiscard]] std::string getLine(std::string_view question);
 };
 
 
